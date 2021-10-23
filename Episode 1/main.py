add074062fe54afc8b12ccbee0adcc31b8f57e34
NixonXC <81410474+NixonXC@users.noreply.github.com>
2021-10-23 23:12:52 +0530

    Update main.py

--- Episode 1/main.py
+++ Episode 1/main.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix="!")
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send("Pong!")
+
+@bot.event
+async def on_ready():
+  print("I am Alive!")
+  
+bot.run("TOKEN")
